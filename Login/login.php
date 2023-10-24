10882204b2ae40990acc6a8576340ebacbcadc43
gabriellafabiao <87137798+gabriellafabiao@users.noreply.github.com>
2023-10-24 15:29:47 -0300

    Add files via upload

---
+++ Login/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/estilo/estilo.css">
+    <title>Document</title>
+</head>
+<body>
+   <div class="container">
+        <div class="login">
+            <h3 class="title">Login</h3>
+            <div class="text-input">
+                <i class="ri-user-fill"></i>
+                <input type="text" placeholder="E-mail">
+            </div>
+            <div class="text-input">
+                <i class="ri-lock-fill"></i>
+                <input type="password" placeholder="Senha">
+            </div>
+            <button class="login-btn"><a href="cadastro.php">ENTRAR</a></button>
+            <a href="#" class="forgot">Esqueceu a senha?</a>
+            <div class="create">
+                <a class="conta" href="#">Não tem conta? clique aqui</a>
+                <i class="ri-arrow-right-fill"></i>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
